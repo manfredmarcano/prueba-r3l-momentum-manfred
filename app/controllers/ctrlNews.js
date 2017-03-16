9b745cce2ee5d7b607c4bfced16d780f85be6bbe
Manfred Marcano <manfredalberto@gmail.com>
2017-03-15 23:59:28 -0400

    final

--- app/controllers/ctrlNews.js
+++ app/controllers/ctrlNews.js
@@ -1,32 +1,51 @@
-app.controller("ctrlNews", ["$scope", "servAPI", function($scope, servAPI){
+app.controller("ctrlNews", ["$scope", function($scope){
 
-    /*
-    servAPI
-        .getAll()
-        .then(function(data) {
-            console.log(data);
-            $scope.api = data.data;
-        })
-        .catch(function(err) {
-        	console.log(err);
-        });
+    $scope.news = [
+        {
+            category: "Chain Store Age",
+            date: "Feb 22, 2016",
+            title: "Billabong taps NetSuite to power omnichannel strategy",
+            image: "assets/images/billabong.jpg",
+            body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non cupiditate nemo ut eius! Cupiditate, tempora, quasi? Ducimus iusto placeat nemo, mollitia repellat! Eligendi veritatis, incidunt. Nisi voluptatum at incidunt quas?"
+        },
+        {
+            category: "Business Wire",
+            date: "Feb 18, 2016",
+            title: "New Study Shows One Critical Industry Lagging Behind in Software Security",
+            image: "",
+            body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non cupiditate nemo ut eius! Cupiditate, tempora, quasi? Ducimus iusto placeat nemo"
+        },
+        {
+            category: "Internet Retailer",
+            date: "Feb 14, 2016",
+            title: "Netsuit rolls out a new e-procurement system for B2B companies",
+            image: "assets/images/netsuite.jpg",
+            body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non cupiditate nemo ut eius! Cupiditate, tempora, quasi? Ducimus iusto placeat nemo, mollitia repellat! Eligendi veritatis, incidunt."
+        },
+        {
+            category: "Business Wire",
+            date: "Feb 13, 2016",
+            title: "AlphaGo and the Declining Advantage of Big Companies",
+            image: "assets/images/alphago.jpg",
+            body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non cupiditate nemo ut eius! Cupiditate, tempora, quasi? Ducimus iusto placeat nemo, mollitia repellat! Eligendi veritatis, incidunt. Nisi voluptatum at incidunt quas?. Nisi voluptatum at incidunt quas?"
+        },
+        {
+            category: "Chain Store Age",
+            date: "Feb 13, 2016",
+            title: "Journelle, Alton Lane stremline process with NetSuite plataform",
+            image: "assets/images/alton.jpg",
+            body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non cupiditate nemo ut eius! Cupiditate, tempora, quasi?"
+        },
+        {
+            category: "The Wall Street Journal",
+            date: "Feb 13, 2016",
+            title: "NetSuite to Buy Email Marketing Company for $200 millions",
+            image: "",
+            body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non cupiditate nemo ut eius! Cupiditate, tempora, quasi? Ducimus iusto placeat nemo, mollitia repellat! Eligendi veritatis, incidunt. Nisi voluptatum at incidunt quas?. Nisi voluptatum at incidunt quas?"
+        }
+       
 
-    $scope.closeAlert = function(index) {
-        $scope.api.splice(index, 1);
-    };
-    */
+    ];
 
-    $scope.mensaje = "News";
-
-
-    servAPI
-        .getAll()
-        .then(function(data) {
-            console.log(data.data);
-            $scope.arreglo = data.data;
-        })
-        .catch(function(err) {
-            console.log(err);
-        });
 
 }]);
